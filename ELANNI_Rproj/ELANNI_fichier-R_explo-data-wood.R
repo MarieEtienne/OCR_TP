9e8c0d444604cfbfbce1370b81e0da3d7df41ef5
Nicolas Chevillon <nchevillon92@gmail.com>
2026-09-16 11:21:29 +0200

    ajout entête editor

--- ELANNI_Rproj/ELANNI_fichier-R_explo-data-wood.R
+++ ELANNI_Rproj/ELANNI_fichier-R_explo-data-wood.R
@@ -4,6 +4,10 @@ date: "2026-09-16 10:20:40 CEST "
 bibliography: references.bib
 execute: 
   freeze: auto
+editor:
+  markdown:
+    canonical: true
+    wrap: 72
 format:
   html:
   theme: cosmo
@@ -12,3 +16,4 @@ toc: true
 toc_float: true
 
 ## ouvrir dataset
+dta <- read.table("data_wood_density.csv", header = TRUE, sep = ",", dec = ".")
