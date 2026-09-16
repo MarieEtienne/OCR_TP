2b0c90da7e414970dabeba5557a2e5246b556c6b
anaelle666 <baron.anaelle@free.fr>
2026-09-16 10:28:09 +0200

    nveau dossier pr Rproj density wood

---
+++ ELANNI_Rproj/ELANNI_fichier-R_explo-data-wood.R
@@ -0,0 +1,14 @@
+title: "Exploration données sur les wood densities"
+author: "BARON Anaëlle" 
+date: "2026-09-16 10:20:40 CEST "
+bibliography: references.bib
+execute: 
+  freeze: auto
+format:
+  html:
+  theme: cosmo
+css: theme.css
+toc: true
+toc_float: true
+
+## ouvrir dataset
